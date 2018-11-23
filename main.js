8ba5d2cdcb549b0e85964a776d2e2c5c65b36867
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-11-23 17:39:43 -0500

    randomly generated shape appears at top of grid

--- main.js
+++ main.js
@@ -5,11 +5,28 @@ for (let i = 0; i < 240; i++) {
     grid.appendChild(square);
 }
 
-let tetrominoShapes = Array.from(document.getElementById('tetrominoes').children);
-for (let shape of tetrominoShapes) {
+let shapeIds = ['i', 'o', 'z', 't', 'l'];
+
+let tetrominoes = shapeIds.map(id => {
+    let shape = document.createElement('div');
+    shape.classList.add('shape');
+    shape.id = id;
     for (let i = 0; i < 4; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
         shape.appendChild(square);
     }
+    return shape;
+});
+
+function getRandomShape() {
+    let shape = tetrominoes[Math.floor(Math.random() * 5)];
+    return shape;
 }
+
+let shape = getRandomShape();
+shape.style.position = 'absolute';
+shape.style.top = '0px';
+shape.style.left = '0px';
+grid.appendChild(shape);
+
