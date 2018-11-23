74b2bd91a0ef4a84058c8354582ce97d63d5d4d5
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-11-23 11:11:12 -0500

    i, o, and z shapes

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 let grid = document.getElementById('grid');
-
 for (let i = 0; i < 240; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     grid.appendChild(square);
+}
+
+let iShape = document.getElementById('i');
+for (let i = 0; i < 4; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    iShape.appendChild(square); 
+}
+
+let oShape = document.getElementById('o');
+for (let i = 0; i < 4; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    oShape.appendChild(square); 
+}
+
+let zShape = document.getElementById('z');
+for (let i = 0; i < 4; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    zShape.appendChild(square); 
 }
