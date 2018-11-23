5a4945cf2050f8101bd58b510a2d4528461ae91c
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-11-23 18:37:47 -0500

    refactored functions

--- main.js
+++ main.js
@@ -1,47 +1,69 @@
-let grid = document.getElementById('grid');
-for (let i = 0; i < 240; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    grid.appendChild(square);
+playGame();
+
+function playGame() {
+    drawGrid();
+    let tetrominoes = createShapes();
+    descendShape(tetrominoes);
 }
 
-let shapeIds = ['i', 'o', 'z', 't', 'l'];
-
-let tetrominoes = shapeIds.map(id => {
-    let shape = document.createElement('div');
-    shape.classList.add('shape');
-    shape.id = id;
-    for (let i = 0; i < 4; i++) {
+function drawGrid() {
+    let grid = document.getElementById('grid');
+    for (let i = 0; i < 240; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
-        shape.appendChild(square);
+        grid.appendChild(square);
     }
-    return shape;
-});
-
-function getRandomShape() {
-    let shape = tetrominoes[Math.floor(Math.random() * 5)];
-    return shape;
 }
 
-let shape = getRandomShape();
-shape.style.position = 'absolute';
-shape.style.top = '0px';
-shape.style.left = '80px';
-grid.appendChild(shape);
+function createShapes() {
+    let shapeIds = ['i', 'o', 'z', 't', 'l'];
 
-floatDown();
+    let tetrominoes = shapeIds.map(id => {
+        let shape = document.createElement('div');
+        shape.classList.add('shape');
+        shape.id = id;
+        for (let i = 0; i < 4; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            shape.appendChild(square);
+        }
+        return shape;
+    });
 
-function floatDown() {
+    return tetrominoes;
+}
+
+function descendShape(arrayOfShapes) {
+    let shape = getRandomShape(arrayOfShapes);
+
+    shape.style.position = 'absolute';
+    shape.style.top = '0px';
+    if (shape.id === 'i') {
+        shape.style.left = '80px';
+    } else {
+        shape.style.left = '100px';
+    }
+    
+    document.getElementById('grid').appendChild(shape);
+    floatDown(shape);
+}
+
+function getRandomShape(arrayOfShapes) {
+    return arrayOfShapes[Math.floor(Math.random() * 5)];
+}
+
+function floatDown(shape) {
     let floatDownInterval = window.setInterval(drop, 500);
     let height = Number(shape.style.top.substring(0, -2));
 
     function drop() {
         height += 20;
-        if (height <= 360) {
+        if (shape.id === 'i' && height <= 380) {
+            shape.style.top = height + 'px';
+        } else if (shape.id !== 'i' && height <= 360) {
             shape.style.top = height + 'px';
         } else {
             clearInterval(floatDownInterval);
         }
     }
-}
+}    
