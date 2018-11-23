5983a021f9136e3e8a0ca6e2dc54bdd47ae2d252
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-11-23 11:23:24 -0500

    for-of loop to add squares to tetrominoes

--- main.js
+++ main.js
@@ -5,37 +5,11 @@ for (let i = 0; i < 240; i++) {
     grid.appendChild(square);
 }
 
-let iShape = document.getElementById('i');
-for (let i = 0; i < 4; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    iShape.appendChild(square); 
-}
-
-let oShape = document.getElementById('o');
-for (let i = 0; i < 4; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    oShape.appendChild(square); 
-}
-
-let zShape = document.getElementById('z');
-for (let i = 0; i < 4; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    zShape.appendChild(square); 
-}
-
-let lShape = document.getElementById('l');
-for (let i = 0; i < 4; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    lShape.appendChild(square); 
-}
-
-let tShape = document.getElementById('t');
-for (let i = 0; i < 4; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    tShape.appendChild(square); 
+let tetrominoShapes = Array.from(document.getElementById('tetrominoes').children);
+for (let shape of tetrominoShapes) {
+    for (let i = 0; i < 4; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        shape.appendChild(square);
+    }
 }
