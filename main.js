25da5517ee08905573c3ec118e4ceecf92e62e2e
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-11-26 18:35:49 -0500

    wip

--- main.js
+++ main.js
@@ -6,31 +6,39 @@ class Game {
 
     initializeBoard() {
         let board = [];
-        for (let i = 0; i < 240; i++) {
-            board.push(null);
+        for (let i = 1; i <= 240; i++) {
+            board.push(i);
         }
         return board;
     }
 
     drawBoard() {
         let grid = document.getElementById('grid');
-        for (let i = 0; i < 240; i++) {
+        for (let i = 1; i <= 240; i++) {
             let square = document.createElement('div');
             square.classList.add('square');
             grid.appendChild(square);
         }
     }
 
-    updateBoard() {
+    updateBoard(occupiedSquares) {
+        let [ first, second, third, fourth ] = occupiedSquares;
+        this.board[first - 1] = 'occupied';
+        this.board[second - 1] = 'occupied';
+        this.board[third - 1] = 'occupied';
+        this.board[fourth - 1] = 'occupied';
+    }
+
+    play() {
         let shape = this.currentShape.shape;
-        this.positionShape(shape);
+        this.currentShape.position();
         drop = drop.bind(this);
-        const floatDownInterval = window.setInterval(() => drop(shape), 70);
+        const floatDownInterval = window.setInterval(() => drop(shape), 7000);
         let height = Number(shape.style.top.substring(0, -2));
     
         function drop(shape) {
             height += 20;
-            let classes = Array.from(shape.classList);
+            let classes = this.currentShape.classes;
             if (classes.includes('i') && height <= (bottom + 20)) {
                 shape.style.top = height + 'px';
             } else if (!classes.includes('i') && height <= bottom) {
@@ -38,6 +46,7 @@ class Game {
             } else {
                 clearInterval(floatDownInterval);
                 this.currentShape.updateState("fixed");
+                this.updateBoard(this.currentShape.calculatePosition());
             }
             if (this.currentShape.state === "fixed") {
                 if (classes.includes('i')) {
@@ -46,25 +55,13 @@ class Game {
                     bottom -= 40;
                 }
                 this.getNewShape();
-                this.updateBoard();
+                this.play();
             }
         }
     }
 
     getNewShape() {
         this.currentShape = new Shape(idIterator.next().value);
-    }
-
-    positionShape(shape) {
-        shape.style.position = 'absolute';
-        shape.style.top = '0px';
-        const classes = Array.from(shape.classList);
-        if (classes.includes('i')) {
-            shape.style.left = '80px';
-        } else {
-            shape.style.left = '100px';
-        }
-        document.getElementById('grid').appendChild(shape);
     }
 }
 
@@ -73,6 +70,7 @@ class Shape {
         this.id = id;
         this.possibleShapes = this.createShapes(this.id);
         this.shape = this.getRandomShape(this.possibleShapes);
+        this.classes = Array.from(this.shape.classList);
         this.state = "moving";
     }
 
@@ -99,8 +97,39 @@ class Shape {
         return possibleShapes[Math.floor(Math.random() * 5)];
     }
 
+    position() {
+        this.shape.style.position = 'absolute';
+        this.shape.style.top = '0px';
+        if (this.classes.includes('i')) {
+            this.shape.style.left = '80px';
+        } else {
+            this.shape.style.left = '100px';
+        }
+        document.getElementById('grid').appendChild(this.shape);
+    }
+
     updateState(state) {
         this.state = state === "moving" ? "moving" : "fixed";
+    }
+
+    calculatePosition() {
+        let pixelsFromTop = Number(this.shape.style.top.substring(0, -2));
+        // let pixelsFromLeft = Number(this.shape.style.left.substring(0, -2));
+        let squaresFromTop = pixelsFromTop / 20;
+        // let squaresFromLeft = pixelsFromLeft / 20;
+        let occupiedSquares;
+        if (this.classes.includes('i')) {
+            occupiedSquares = [233, 234, 235, 236];
+        } else if (this.classes.includes('o')) {
+            occupiedSquares = [222, 223, 234, 235];
+        } else if (this.classes.includes('z')) {
+            occupiedSquares = [222, 223, 235, 236];
+        } else if (this.classes.includes('t')) {
+            occupiedSquares = [222, 223, 224, 235];
+        } else if (this.classes.includes('l')) {
+            occupiedSquares = [222, 223, 224, 234];
+        }
+        return occupiedSquares;
     }
 }
 
@@ -115,7 +144,6 @@ function* IdGenerator() {
 const idIterator = IdGenerator();
 let bottom = 360;
 
-let g = new Game();
-g.initializeBoard();
-g.drawBoard();
-g.updateBoard();
+let game = new Game();
+game.drawBoard();
+game.play();
