02582a5d8cb4c91926c79e577d5f47fc00149d37
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-11-26 17:24:24 -0500

    refactored Round class into Game class and Shape class

--- main.js
+++ main.js
@@ -1,9 +1,79 @@
-class Round {
+class Game {
+    constructor() {
+        this.board = this.initializeBoard();
+        this.currentShape = new Shape(idIterator.next().value);
+    }
+
+    initializeBoard() {
+        let board = [];
+        for (let i = 0; i < 240; i++) {
+            board.push(null);
+        }
+        return board;
+    }
+
+    drawBoard() {
+        let grid = document.getElementById('grid');
+        for (let i = 0; i < 240; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            grid.appendChild(square);
+        }
+    }
+
+    updateBoard() {
+        let shape = this.currentShape.shape;
+        this.positionShape(shape);
+        drop = drop.bind(this);
+        const floatDownInterval = window.setInterval(() => drop(shape), 70);
+        let height = Number(shape.style.top.substring(0, -2));
+    
+        function drop(shape) {
+            height += 20;
+            let classes = Array.from(shape.classList);
+            if (classes.includes('i') && height <= (bottom + 20)) {
+                shape.style.top = height + 'px';
+            } else if (!classes.includes('i') && height <= bottom) {
+                shape.style.top = height + 'px';
+            } else {
+                clearInterval(floatDownInterval);
+                this.currentShape.updateState("fixed");
+            }
+            if (this.currentShape.state === "fixed") {
+                if (classes.includes('i')) {
+                    bottom -= 20;
+                } else {
+                    bottom -= 40;
+                }
+                this.getNewShape();
+                this.updateBoard();
+            }
+        }
+    }
+
+    getNewShape() {
+        this.currentShape = new Shape(idIterator.next().value);
+    }
+
+    positionShape(shape) {
+        shape.style.position = 'absolute';
+        shape.style.top = '0px';
+        const classes = Array.from(shape.classList);
+        if (classes.includes('i')) {
+            shape.style.left = '80px';
+        } else {
+            shape.style.left = '100px';
+        }
+        document.getElementById('grid').appendChild(shape);
+    }
+}
+
+class Shape {
     constructor(id) {
         this.id = id;
-        this.shapes = this.createShapes(this.id);
-        this.shape = this.getRandomShape(this.shapes);
-        this.shapeAtBottom = false;
+        this.possibleShapes = this.createShapes(this.id);
+        this.shape = this.getRandomShape(this.possibleShapes);
+        this.state = "moving";
     }
 
     createShapes(id) {
@@ -25,61 +95,12 @@ class Round {
         return tetrominoes;
     }
 
-    getRandomShape(array) {
-        return array[Math.floor(Math.random() * 5)];
+    getRandomShape(possibleShapes) {
+        return possibleShapes[Math.floor(Math.random() * 5)];
     }
 
-    positionShape() {
-        this.shape.style.position = 'absolute';
-        this.shape.style.top = '0px';
-        let classes = Array.from(this.shape.classList);
-        if (classes.includes('i')) {
-            this.shape.style.left = '80px';
-        } else {
-            this.shape.style.left = '100px';
-        }
-    }
-
-    floatDown() {
-        drop = drop.bind(this);
-        let floatDownInterval = window.setInterval(() => drop(this.shape), 70);
-        let height = Number(this.shape.style.top.substring(0, -2));
-    
-        function drop(shape) {
-            height += 20;
-            let classes = Array.from(shape.classList);
-            if (classes.includes('i') && height <= (bottom + 20)) {
-                shape.style.top = height + 'px';
-            } else if (!classes.includes('i') && height <= bottom) {
-                shape.style.top = height + 'px';
-            } else {
-                clearInterval(floatDownInterval);
-                this.shapeAtBottom = true;
-            }
-            if (this.shapeAtBottom) {
-                if (classes.includes('i')) {
-                    bottom -= 20;
-                } else {
-                    bottom -= 40;
-                }
-                play();
-            }
-        }
-    }      
-
-    playRound() {
-        this.positionShape();
-        document.getElementById('grid').appendChild(this.shape);
-        this.floatDown();
-    }  
-}
-
-function drawGrid() {
-    let grid = document.getElementById('grid');
-    for (let i = 0; i < 240; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
+    updateState(state) {
+        this.state = state === "moving" ? "moving" : "fixed";
     }
 }
 
@@ -90,14 +111,11 @@ function* IdGenerator() {
     }
 }
 
-function play() {
-    let r = new Round(idIterator.next().value);
-    r.playRound();
-}
-
 
 const idIterator = IdGenerator();
 let bottom = 360;
 
-drawGrid();
-play();
+let g = new Game();
+g.initializeBoard();
+g.drawBoard();
+g.updateBoard();
