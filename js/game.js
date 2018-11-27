7a86d028b84835295b372b3361d7b3c8b0e1b182
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-11-27 13:01:00 -0500

    prevent left and right movement once shape has touched bottom

--- js/game.js
+++ js/game.js
@@ -8,12 +8,13 @@ class Game {
         this.currentShape.positionSelf();
         drop = drop.bind(this);
 
-        const floatDownInterval = window.setInterval(() => drop(this.currentShape.shape), 600);
+        const floatDownInterval = window.setInterval(() => drop(this.currentShape.shape), 1050);
         const classes = this.currentShape.classes;
         let height = Number(this.currentShape.shape.style.top.slice(0, -2));
+        console.log('classes:', classes, 'height:', height, 'bottom:', bottom);
     
         function drop(shape) {
-            height += 20;
+            height += 20; console.log('height again:', height);
             let squaresToMoveTo = this.currentShape.getNextPositionGoingDown();
             let keepMoving = true;
 
@@ -22,7 +23,9 @@ class Game {
                     keepMoving = false;
                 }
             }
-            if (classes.includes('i') && height > (bottom + 20)) {
+            if (this.currentShape.state === 'fixed') {
+                keepMoving = false;
+            } else if (classes.includes('i') && height > (bottom + 20)) {
                 keepMoving = false;
             } else if (!classes.includes('i') && height > bottom) {
                 keepMoving = false;
@@ -32,14 +35,24 @@ class Game {
                 clearInterval(floatDownInterval);
                 this.currentShape.updateState('fixed');
                 this.board.update(this.currentShape.position);
+
+                if (this.currentShape.state === 'fixed') {
+                    this.getNewShape();
+                    this.play();
+                }
             } else {
                 shape.style.top = height + 'px';
                 this.currentShape.position = squaresToMoveTo;
-            }
+                if (this.shapeShouldBecomeFixed()) {
+                    clearInterval(floatDownInterval);
+                    this.currentShape.updateState('fixed');
+                    this.board.update(this.currentShape.position);
+                }
 
-            if (this.currentShape.state === 'fixed') {
-                this.getNewShape();
-                this.play();
+                if (this.currentShape.state === 'fixed') {
+                    this.getNewShape();
+                    this.play();
+                }
             }
         }
     }
@@ -52,22 +65,20 @@ class Game {
                 keepMoving = false;
             }
         }
-
         if (this.currentShape.state === 'fixed') {
             keepMoving = false;
         }
-        // let height = Number(this.currentShape.shape.style.top.slice(0, -2));
-        // if (this.currentShape.classes.includes('i') && height > (bottom + 20)) {
-        //     keepMoving = false;
-        // } else if (!this.currentShape.classes.includes('i') && height > bottom) {
-        //     keepMoving = false;
-        // }
 
         if (keepMoving) {
             let xPos = Number(this.currentShape.shape.style.left.slice(0, -2));
             xPos -= 20;
             this.currentShape.shape.style.left = xPos + 'px';
             this.currentShape.position = squaresToMoveTo;
+
+            if (this.shapeShouldBecomeFixed()) {
+                this.currentShape.updateState('fixed');
+                this.board.update(this.currentShape.position);
+            }
         }
     }
 
@@ -84,7 +95,28 @@ class Game {
             xPos += 20;
             this.currentShape.shape.style.left = xPos + 'px';
             this.currentShape.position = squaresToMoveTo;
+
+            if (this.shapeShouldBecomeFixed()) {
+                this.currentShape.updateState('fixed');
+                this.board.update(this.currentShape.position);
+            }
         }
+    }
+
+    shapeShouldBecomeFixed() {
+        let squaresToMoveToNext = this.currentShape.getNextPositionGoingDown();
+        for (let squareNumber of squaresToMoveToNext) {
+            if (this.board.squares[squareNumber - 1] === 'occupied') {
+                return true;
+            }
+        }
+        let height = Number(this.currentShape.shape.style.top.slice(0, -2)) + 20;
+        if (this.currentShape.classes.includes('i') && height > (bottom + 20)) {
+            return true;
+        } else if (!this.currentShape.classes.includes('i') && height > bottom) {
+            return true;
+        }
+        return false;
     }
 
     getNewShape() {
